442b9275a3ef68f268356ad59d81d3bae7b4151f
Ahmet Behdioğlu <ahmetuestcostaj@gmail.com>
2025-05-11 12:08:26 +0300

    band name generator demo completed

---
+++ PythonProject2_AY1_BandNameGenerator/PythonProject2_AY1_BandNameGenerator.py
@@ -0,0 +1,5 @@
+
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grow up in?..: \n")
+petAnimal = input("What is your pet's name?..: \n")
+print(f"Your band name could be {city} {petAnimal}")
