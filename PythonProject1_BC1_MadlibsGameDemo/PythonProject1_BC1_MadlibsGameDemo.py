9cc885fe73306f25fa2d3aa16ac878fa74fd9c6e
Ahmet Behdioğlu <ahmetuestcostaj@gmail.com>
2025-05-11 10:56:42 +0300

    madlibs demo completed

---
+++ PythonProject1_BC1_MadlibsGameDemo/PythonProject1_BC1_MadlibsGameDemo.py
@@ -0,0 +1,14 @@
+# Madlibs Game 
+# Word game where you generate a story
+# By filling in blanks with random words 
+
+adjectiveOne = input("Enter an adjective (description)..: ")
+nounOne = input("Enter a noun (person, placae, thing)..: ")
+verbOne = input("Enter a verb ending with 'ing'..: ")
+adjectiveTwo = input("Enter another adjective (description)..: ")
+adjectiveThree = input("Enter another adjective (description)..: ")
+
+print(f"Today i went to a {adjectiveOne} zoo.")
+print(f"In an exhibit, i saw a {nounOne}")
+print(f"{nounOne} was {adjectiveTwo} and {verbOne}")
+print(f"I was {adjectiveThree}!")
